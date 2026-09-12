d14a641843f80cd57b59c6d9c7869a7782d61ad3
Milan Todorovic <todor-90@hotmail.com>
2026-09-12 21:29:38 +0200

    Update build-jetson-image.sh

--- scripts/build-jetson-image.sh
+++ scripts/build-jetson-image.sh
@@ -10,7 +10,7 @@ l4t=32
 supported_boards=("jetson-nano" "jetson-nano-2gb")
 
 function usage() {
-    echo "Usage: $0 -b <board> -r <revision> -d <device>"
+    echo "Usage: $0 -b <board> -r <revision>"
     echo ""
     echo "board: the board name, one of the following names:"
     for supported_board in "${supported_boards[@]}"; do
@@ -18,8 +18,6 @@ function usage() {
     done
     echo ""
     echo "revision: the revision number. The possible values are: 100, 200 or 300."
-    echo ""
-    echo "device: B01"
     echo ""
     echo "l4t version. The possible value is: 32"
     exit 1
@@ -98,7 +96,6 @@ sudo podman run \
     --privileged \
     -v .:/jetson \
     -e JETSON_BOARD="$board" \
-    -e JETSON_DEVICE="$device" \
     -e JETSON_REVISION="$revision" \
     localhost/jetson-build-image-l4t"$l4t":latest \
     create-jetson-image.sh
